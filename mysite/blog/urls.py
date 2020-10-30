f357a63a1b94dccc8f7e8f8fc41c6c49e8bba283
samrat101 <samratrao101@gmail.com>
2020-10-30 10:32:13 +0530

    Views and Base Html

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -9,4 +9,8 @@ urlpatterns=[
     url(r'^post/(?P<ok>\d+)/edit/$',views.PostUpdateView.as_view(),name='post_edit'),
     url(r'^post/(?P<ok>\d+)/remove/$',views.PostDeleteView.as_view(),name='post_remove'),
     url(r'^drafts/$',views.DraftListView.as_view(),name='post_draft_list'),
+    url(r'post/(?P<ok>\d+)/comment/$',views.add_comment_to_post(),name='add_comment_to_post'),
+    url(r'comment/(?P<ok>\d+)/approve/$',views.comment_approve(),name='comment_approve'),
+    url(r'comment/(?P<ok>\d+)/remove/$',views.comment_remove(),name='comment_remove'),
+    url(r'post/(?P<ok>\d+)/publish/$',views.post_publish(),name='post_publish'),
 ]
